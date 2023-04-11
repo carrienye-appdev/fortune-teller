8c8bd10b241ccd141c5d00b679c8292348a3517f
carrie nye <cnye@chicagobooth.edu>
2023-04-11 14:02:33 +0000

    aries

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -10,4 +10,16 @@ class NumbersController < ApplicationController
 
     render({ :template => "lottery_stuff/woohoo.html.erb"})
   end 
+
+  def losers
+    @koala = Array.new
+
+    5.times do
+      giraffe = rand(1...100)
+      
+      @koala.push(giraffe)
+    end
+
+    render({ :template => "lottery_stuff/losers.html.erb"})
+  end 
 end
